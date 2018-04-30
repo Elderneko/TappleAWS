eba1b6c782ea1b04f84eb7e54b36a320777dcf4d
Cegoco11 <32985890+Cegoco11@users.noreply.github.com>
2018-04-30 18:21:47 +0200

    Login
    Modificados algunos metodos para poder tener toda la informacion necesaria en las siguientes actividades.
    Modificados algunos layouts que venian por defecto para que se ajusten mas a nuestro proyecto.
    Agregados metodos de guardar info en el SharedPreferences

--- app/src/main/java/com/example/cegoc/tapple/LoginActivity.java
+++ app/src/main/java/com/example/cegoc/tapple/LoginActivity.java
@@ -1,14 +1,17 @@
 package com.example.cegoc.tapple;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import cad.Login;
+import cad.Teacher;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,7 +19,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edt_user, edt_pass;
     private Intent change;
 
-    private class Tarea extends android.os.AsyncTask<Void, Integer, Integer> {
+    private class Tarea extends android.os.AsyncTask<Void, Teacher, Teacher> {
 
         @Override
         protected void onPreExecute() {
@@ -25,24 +28,36 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         @Override
-        protected Integer doInBackground(Void... voids) {
+        protected Teacher doInBackground(Void... voids) {
             cad.TappleCAD t = new cad.TappleCAD();
-            int id = t.checkLogin(edt_user.getText().toString(), edt_pass.getText().toString());
-            return id;
+            int id_teacher = t.checkLogin(edt_user.getText().toString(), edt_pass.getText().toString());
+            // Si la id es 0, no existe el usuario
+            if(id_teacher != 0){
+                return t.showProfile(id_teacher);
+            } else{
+                return null;
+            }
         }
 
         @Override
-        protected void onPostExecute(Integer aInteger) {
-            super.onPostExecute(aInteger);
-            if(aInteger != 0){
+        protected void onPostExecute(Teacher t) {
+            super.onPostExecute(t);
+            // Si no existe el usuario no se hace otra llamada a la BD
+            if(t != null){
                 // Iniciando sesion...
-                feedback.setText("Usuario encontrado (id="+aInteger+")");
+                saveInfo(t.getId_teacher(), t.getUser());
                 startActivity(change);
                 finishAffinity();
             } else {
-                feedback.setText("Usuario no encontrado (id="+aInteger+")");
                 // Credenciales incorrectas
+                Toast.makeText(LoginActivity.this,
+                        "Credenciales incorrectos",Toast.LENGTH_LONG).show();
             }
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
         }
     }
 
@@ -63,5 +78,20 @@ public class LoginActivity extends AppCompatActivity {
                     new Tarea().execute();
             }
         });
+    }
+
+    /**
+     * Guarda en el shared preferences una id y un nombre
+     *
+     * @param id_param id a guardar
+     * @param user nombre de usuario a guardar
+     */
+    private void saveInfo(int id_param, String user){
+        SharedPreferences preferencias=getSharedPreferences("TEACHER_INFO",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.putInt("ID_TEACHER", id_param);
+        editor.putString("USER_TEACHER", user);
+        editor.apply();
     }
 }
--- app/src/main/java/com/example/cegoc/tapple/MainMenu.java
+++ app/src/main/java/com/example/cegoc/tapple/MainMenu.java
@@ -1,17 +1,17 @@
 package com.example.cegoc.tapple;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -30,6 +30,8 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        loadUsername();
     }
 
     @Override
@@ -48,22 +50,29 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_profile) {
+            // ToDo Ir a la actividad de perfil
+        } else if (id == R.id.nav_students) {
+            // ToDo Ir a la actividad de estudiantes
+        } else if (id == R.id.nav_meeting) {
+            // ToDo Ir a la actividad de citas
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * Carga el nombre del profesor que hemos guardado en el shared preferences
+     * en el menu lateral
+     */
+    private void loadUsername(){
+        NavigationView nv = findViewById(R.id.nav_view);
+        View headerView = nv.getHeaderView(0);
+        TextView tv = headerView.findViewById(R.id.txt_drawer_username);
+        String aux = getSharedPreferences("TEACHER_INFO", Context.MODE_PRIVATE).
+                getString("USER_TEACHER","");
+        tv.setText(aux);
     }
 }
