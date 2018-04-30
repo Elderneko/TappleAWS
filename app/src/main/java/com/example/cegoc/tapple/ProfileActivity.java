a50aee578d0a40c8b40742b44ebfbf11eb1aa86e
Cegoco11 <32985890+Cegoco11@users.noreply.github.com>
2018-04-30 22:06:08 +0200

    Perfil Profesor
    Se ha creado una actividad de perfil, en la que se muestran los datos del profesor actual mediante las consultas del CAD. Asi mismo se puede acceder a ella desde el menu de navegacion lateral.

--- app/src/main/java/com/example/cegoc/tapple/ProfileActivity.java
+++ app/src/main/java/com/example/cegoc/tapple/ProfileActivity.java
@@ -11,14 +11,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import cad.Teacher;
+
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private class Tarea extends android.os.AsyncTask<Void, Teacher, Teacher> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //ToDo Estaria bien avisar de que se esta haciendo algo y que hay que esperar
+        }
+
+        @Override
+        protected Teacher doInBackground(Void... voids) {
+            cad.TappleCAD t = new cad.TappleCAD();
+            int id_teacher=getTeacherID();
+            // Si la id es 0, no existe el usuario
+            if(id_teacher != 0){
+                return t.showProfile(id_teacher);
+            } else{
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Teacher t) {
+            super.onPostExecute(t);
+            // Si no existe el usuario no se hace otra llamada a la BD
+            if(t != null){
+                LinearLayout layout = findViewById(R.id.layout_profile);
+                TextView tName = layout.findViewById(R.id.txt_profile_name);
+                TextView tSurnames = layout.findViewById(R.id.txt_profile_surnames);
+                TextView tEmail = layout.findViewById(R.id.txt_profile_email);
+                TextView tBirthday = layout.findViewById(R.id.txt_profile_birthday);
+                tName.setText(t.getName());
+                tSurnames.setText(t.getSurname1() + " " + t.getSurname2());
+                tEmail.setText(t.getEmail());
+                tBirthday.setText(t.getBirthday().toString());
+            } else {
+
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -32,6 +80,7 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         navigationView.setNavigationItemSelectedListener(this);
 
         loadUsername();
+        new Tarea().execute();
     }
 
     @Override
@@ -75,5 +124,15 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         String aux = getSharedPreferences("TEACHER_INFO", Context.MODE_PRIVATE).
                 getString("USER_TEACHER","");
         tv.setText(aux);
+    }
+
+    /**
+     * Devuelve la id del profesor almacenada en el shared preferences
+     *
+     * @return int id
+     */
+    private int getTeacherID(){
+        return getSharedPreferences("TEACHER_INFO", Context.MODE_PRIVATE).
+                getInt("ID_TEACHER",0);
     }
 }
