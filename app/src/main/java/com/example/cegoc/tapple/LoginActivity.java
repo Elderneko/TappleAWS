c21f05cd1e370ccc792c4778c456383d6ea2de0b
Cegoco11 <32985890+Cegoco11@users.noreply.github.com>
2018-05-01 20:35:09 +0200

    Restore
    Se ha subido el mismo proyecto rehecho, para evitar posibles corrupciones, y ficheros temporales que podrian molestar.
    Se ha de comprobar minuciosamente, que todo funciona correctamente antes de seguir desarrollando.

---
+++ app/src/main/java/com/example/cegoc/tapple/LoginActivity.java
@@ -0,0 +1,109 @@
+package com.example.cegoc.tapple;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import cad.Teacher;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edt_user, edt_pass;
+    private Intent change;
+
+    private class Tarea extends android.os.AsyncTask<Void, Teacher, Teacher> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //ToDo Estaria bien avisar de que se esta haciendo algo y que hay que esperar
+        }
+
+        @Override
+        protected Teacher doInBackground(Void... voids) {
+            cad.TappleCAD t = new cad.TappleCAD();
+            int id_teacher = t.checkLogin(edt_user.getText().toString(), edt_pass.getText().toString());
+            // Si la id es 0, no existe el usuario
+            if(id_teacher != 0){
+                return t.showProfile(id_teacher);
+            } else{
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Teacher t) {
+            super.onPostExecute(t);
+            // Si no existe el usuario no se hace otra llamada a la BD
+            if(t != null){
+                // Iniciando sesion...
+                saveInfo(t.getId_teacher(), t.getUser());
+                startActivity(change);
+                finishAffinity();
+            } else {
+                // Credenciales incorrectas
+                Toast.makeText(LoginActivity.this,
+                        "Credenciales incorrectos",Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        change = new Intent(this, MainMenu.class);
+        edt_user = findViewById(R.id.edt_user);
+        edt_pass = findViewById(R.id.edt_pass);
+        Button bLogin = findViewById(R.id.btn_login);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Tarea().execute();
+            }
+        });
+    }
+
+    /**
+     * Guarda en el shared preferences una id y un nombre
+     *
+     * @param id_param id a guardar
+     * @param user nombre de usuario a guardar
+     */
+    private void saveInfo(int id_param, String user){
+        SharedPreferences preferencias=getSharedPreferences("TEACHER_INFO",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.putInt("ID_TEACHER", id_param);
+        editor.putString("USER_TEACHER", user);
+        editor.apply();
+    }
+
+    /**
+     * Lleva al formulario de registro
+     *
+     * @param v
+     */
+    public void goRegister(View v){
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+//    public void goForgetPass(View v){
+//        startActivity(new Intent(this, ForgetActivity.class));
+//    }
+
+}
---
+++ app/src/main/java/com/example/cegoc/tapple/MainMenu.java
@@ -0,0 +1,83 @@
+package com.example.cegoc.tapple;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        loadUsername();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            startActivity(new Intent(this, MainMenu.class));
+            finishAffinity();
+        } else if (id == R.id.nav_profile) {
+            startActivity(new Intent(this, ProfileActivity.class));
+            finishAffinity();
+        } else if (id == R.id.nav_students) {
+            startActivity(new Intent(this, StudentsActivity.class));
+            finishAffinity();
+        } else if (id == R.id.nav_meeting) {
+            // ToDo Ir a la actividad de citas
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    /**
+     * Carga el nombre del profesor que hemos guardado en el shared preferences
+     * en el menu lateral
+     */
+    private void loadUsername(){
+        NavigationView nv = findViewById(R.id.nav_view);
+        View headerView = nv.getHeaderView(0);
+        TextView tv = headerView.findViewById(R.id.txt_drawer_username);
+        String aux = getSharedPreferences("TEACHER_INFO", Context.MODE_PRIVATE).
+                getString("USER_TEACHER","");
+        tv.setText(aux);
+    }
+}
