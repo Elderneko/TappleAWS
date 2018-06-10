4fa6857d73967200f06253ae68a2482d087cf3db
Cegoco11 <32985890+Cegoco11@users.noreply.github.com>
2018-06-10 11:12:15 +0200

    Listado Citas
    Preparado a falta de revision el listado de las citas.
    Falta testear y darle alguna vuelta.
    He dejado algun comentario de ToDo

--- app/src/main/java/com/example/cegoc/tapple/MeetingList.java
+++ app/src/main/java/com/example/cegoc/tapple/MeetingList.java
@@ -6,6 +6,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,14 +16,61 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
+
+import cad.Meeting;
+import cad.Student;
 
 public class MeetingList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ProgressBar pb;
+
+    private class BackTaskDB extends android.os.AsyncTask<Void, ArrayList<Meeting>, ArrayList<Meeting>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pb.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected ArrayList<Meeting> doInBackground(Void... voids) {
+            cad.TappleCAD t = new cad.TappleCAD();
+            int id_teacher=getTeacherID();
+            // Si la id es 0, no existe el usuario
+            if(id_teacher != 0){
+                return t.showAllMeetings(id_teacher);
+            } else{
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Meeting> list) {
+            super.onPostExecute(list);
+            pb.setVisibility(View.GONE);
+            // Si no existe el usuario no se hace otra llamada a la BD
+            if(list.size() != 0){
+                for(Meeting m : list){
+                    createViewMeeting(m);
+                }
+            } else {
+                //ToDo No tiene alumnos
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            pb.setVisibility(View.GONE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +136,36 @@ public class MeetingList extends AppCompatActivity implements NavigationView.OnN
     }
 
     /**
+     * Crea una linea con el nombre y apellidos del alumno y su onclick
+     */
+    private void createViewMeeting(Meeting m){
+        LinearLayout sv = findViewById(R.id.linear_meetings);
+        LinearLayout auxLinear = new LinearLayout(this);
+        auxLinear.setOrientation(LinearLayout.HORIZONTAL);
+        auxLinear.setLayoutParams(new LinearLayout.LayoutParams
+                (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        auxLinear.setPadding(toDp(10, auxLinear),toDp(10, auxLinear),
+                toDp(10, auxLinear),toDp(10, auxLinear));
+        TextView auxText = new TextView(this);
+        auxText.setGravity(Gravity.CENTER);
+        //ToDo Solo la fecha de la cita o tambien el nombre del alumno?
+        auxText.setText(m.getMeeting_date() + " " );
+        auxLinear.setTag(m.getId_meet());
+        auxLinear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = (int) v.getTag();
+                //ToDo Quitar comentario cuando se cree la actividad de MeetingDetails
+//                Intent i = new Intent(MeetingList.this, MeetingDetails.class);
+//                i.putExtra("ID_MEET", id);
+//                startActivity(i);
+            }
+        });
+        auxLinear.addView(auxText);
+        sv.addView(auxLinear);
+    }
+
+    /**
      * Carga el nombre del profesor que hemos guardado en el shared preferences
      * en el menu lateral
      */
@@ -98,5 +176,26 @@ public class MeetingList extends AppCompatActivity implements NavigationView.OnN
         String aux = getSharedPreferences("TEACHER_INFO", Context.MODE_PRIVATE).
                 getString("USER_TEACHER","");
         tv.setText(aux);
+    }
+
+    /**
+     * Devuelve la id del profesor almacenada en el shared preferences
+     *
+     * @return int id
+     */
+    private int getTeacherID(){
+        return getSharedPreferences("TEACHER_INFO", Context.MODE_PRIVATE).
+                getInt("ID_TEACHER",0);
+    }
+
+    /**
+     * Este metodo pasa de pixeles a dp
+     *
+     * @param num numero de pixeles
+     * @return num transformado a dp
+     */
+    private int toDp(int num, LinearLayout lea){
+        float factor = lea.getResources().getDisplayMetrics().density;
+        return (int) factor*num;
     }
 }
